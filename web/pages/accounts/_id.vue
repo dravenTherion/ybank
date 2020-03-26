c1929a813868c529d75fa16b45147e986a77c787
dravenTherion <i.am.quitoriano@gmail.com>
2020-03-26 14:16:35 +0800

    + Created a displayTransaction method for _id.vue to consolidate the repeating display loop for the transactions

--- web/pages/accounts/_id.vue
+++ web/pages/accounts/_id.vue
@@ -10,59 +10,49 @@
           </div>
           <div>
             Balance:
-            <code
-              >{{ account.currency === "usd" ? "$" : "€"
-              }}{{ account.balance }}</code
-            >
+            <code>
+              {{
+ account.currency === "usd" ? "$" : "€"
+              }}{{ account.balance }}
+            </code>
           </div>
         </b-card-text>
-        <b-button size="sm" variant="success" @click="show = !show"
-          >New payment</b-button
-        >
+        <b-button size="sm" variant="success" @click="show = !show">New payment</b-button>
 
-        <b-button
-          class="float-right"
-          variant="danger"
-          size="sm"
-          nuxt-link
-          to="/"
-          >Logout</b-button
-        >
+        <b-button class="float-right"
+                  variant="danger"
+                  size="sm"
+                  nuxt-link
+                  to="/">Logout</b-button>
       </b-card>
 
       <b-card class="mt-3" header="New Payment" v-show="show">
         <b-form @submit="onSubmit">
           <b-form-group id="input-group-1" label="To:" label-for="input-1">
-            <b-form-input
-              id="input-1"
-              size="sm"
-              v-model="payment.to"
-              type="number"
-              required
-              placeholder="Destination ID"
-            ></b-form-input>
+            <b-form-input id="input-1"
+                          size="sm"
+                          v-model="payment.to"
+                          type="number"
+                          required
+                          placeholder="Destination ID"></b-form-input>
           </b-form-group>
 
           <b-form-group id="input-group-2" label="Amount:" label-for="input-2">
             <b-input-group prepend="$" size="sm">
-              <b-form-input
-                id="input-2"
-                v-model="payment.amount"
-                type="number"
-                required
-                placeholder="Amount"
-              ></b-form-input>
+              <b-form-input id="input-2"
+                            v-model="payment.amount"
+                            type="number"
+                            required
+                            placeholder="Amount"></b-form-input>
             </b-input-group>
           </b-form-group>
 
           <b-form-group id="input-group-3" label="Details:" label-for="input-3">
-            <b-form-input
-              id="input-3"
-              size="sm"
-              v-model="payment.details"
-              required
-              placeholder="Payment details"
-            ></b-form-input>
+            <b-form-input id="input-3"
+                          size="sm"
+                          v-model="payment.details"
+                          required
+                          placeholder="Payment details"></b-form-input>
           </b-form-group>
 
           <b-button type="submit" size="sm" variant="primary">Submit</b-button>
@@ -80,7 +70,8 @@
 import axios from "axios";
 import Vue from "vue";
 
-export default {
+  export default {
+
   data() {
     return {
       show: false,
@@ -117,32 +108,19 @@ export default {
         }/transactions`
       )
       .then(function(response) {
-        that["transactions"] = response.data;
 
-        var transactions = [];
-        for (let i = 0; i < that.transactions.length; i++) {
-          that.transactions[i].amount =
-            (that.account.currency === "usd" ? "$" : "€") +
-            that.transactions[i].amount;
-
-          if (that.account.id != that.transactions[i].to) {
-            that.transactions[i].amount = "-" + that.transactions[i].amount;
-          }
-
-          transactions.push(that.transactions[i]);
-        }
-
-        that.transactions = transactions;
+        that.displayTransactions(response);
 
         if (that.account && that.transactions) {
           that.loading = false;
         }
       });
+
   },
 
   methods: {
     onSubmit(evt) {
-      var that = this;
+      const that = this;
 
       evt.preventDefault();
 
@@ -174,26 +152,40 @@ export default {
             `http://localhost:8000/api/accounts/${
               that.$route.params.id
             }/transactions`
-          )
-          .then(function(response) {
-            that["transactions"] = response.data;
-
-            var transactions = [];
-            for (let i = 0; i < that.transactions.length; i++) {
-              that.transactions[i].amount =
-                (that.account.currency === "usd" ? "$" : "€") +
-                that.transactions[i].amount;
-
-              if (that.account.id != that.transactions[i].to) {
-                that.transactions[i].amount = "-" + that.transactions[i].amount;
-              }
-
-              transactions.push(that.transactions[i]);
-            }
-
-            that.transactions = transactions;
-          });
+        )
+          .then(that.displayTransactions);
       }, 200);
+    },
+
+
+    /**
+     * Display recent transactions
+     * 
+     * @param response
+     */
+    displayTransactions(response) {
+
+      const that = this,
+            transactions = [];
+
+      that["transactions"] = response.data;
+
+      for (let i = 0; i < that.transactions.length; i++) {
+
+        that.transactions[i].amount =
+        (that.account.currency === "usd" ? "$" : "€") +
+        that.transactions[i].amount;
+
+        if (that.account.id != that.transactions[i].to) {
+              that.transactions[i].amount = "-" + that.transactions[i].amount;
+        }
+
+        transactions.push(that.transactions[i]);
+
+      }
+
+      that.transactions = transactions;
+
     }
   }
 };
