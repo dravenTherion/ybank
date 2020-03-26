9777a633529b4788bc8bfdf49b89bdc783cf67c8
dravenTherion <i.am.quitoriano@gmail.com>
2020-03-26 10:28:32 +0800

    + Added Accounts Controller to handle account related operations

---
+++ api/app/Http/Controllers/Accounts.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+
+class Accounts extends Controller{
+
+    /**
+     * get user account based on id    
+     */
+
+    public function get($id){
+
+        $account = DB::table('accounts')
+             ->whereRaw("id=$id")
+             ->get();
+
+        return $account;
+        
+    }
+
+}
