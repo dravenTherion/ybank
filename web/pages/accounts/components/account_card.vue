1faca747ef6195dd1fab5d413caa2f576e3d63fa
dravenTherion <i.am.quitoriano@gmail.com>
2020-03-26 16:57:01 +0800

    + new uploads, account_card component and payment_history_card component

---
+++ web/pages/accounts/components/account_card.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <b-card :header="'Welcome, ' + account.name" class="mt-3">
+    <b-card-text>
+      <div>
+        Account: <code>{{ account.id }}</code>
+      </div>
+      <div>
+        Balance:
+        <code>
+          {{
+ account.currency === "usd" ? "$" : "€"
+          }}{{ account.balance }}
+        </code>
+      </div>
+    </b-card-text>
+    <b-button size="sm" variant="success" v-on:click="$emit('toggle-payment')">New payment</b-button>
+
+    <b-button class="float-right"
+              variant="danger"
+              size="sm"
+              nuxt-link
+              to="/">Logout</b-button>
+  </b-card>
+
+</template>
+
+<script>
+
+  export default {
+
+    name: 'AccountCard',
+
+    props: {
+      account: Object
+    }
+
+  }
+
+</script>
---
+++ web/pages/accounts/components/payment_history_card.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <b-card class="mt-3" header="Payment History">
+    <b-table striped hover :items="transactions.items"></b-table>
+  </b-card>
+
+</template>
+
+<script>
+
+  export default {
+
+    name: 'PaymentHistoryCard',
+
+    props: {
+      transactions: Object
+    }
+
+  }
+
+</script>
